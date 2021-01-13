b98720845ed587e0ed31ed29a9b7d1c12fbabe91
Xiang <1014599072@qq.com>
2021-01-13 10:58:43 +0800

    xnzh

--- js/axios_cfg.js
+++ js/axios_cfg.js
@@ -1,4 +1,13 @@
 //在跨域请求时，会携带用户凭证
-axios.defaults.withCredentials=true;
+axios.defaults.withCredentials = true;
 //请求超时时间,毫秒
-axios.defaults.timeout=10000;
+axios.defaults.timeout = 10000;
+//响应拦截器
+axios.interceptors.response.use(resp => {
+	if(resp.data.code == "300") {
+		location.href = "sessionerror.html";
+	}
+	return resp;
+}, error => {
+	Promise.reject(error);
+})
